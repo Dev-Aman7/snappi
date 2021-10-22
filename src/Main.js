5df440f267026560c9eb4b8656491c5276d40743
Aman <a8285443826@gmail.com>
2021-10-23 01:51:06 +0530

    push code

--- src/Main.js
+++ src/Main.js
@@ -1,40 +1,36 @@
 import { useState } from "react";
-import "./App.css";
 import Box from "@mui/material/Box";
 import Tab from "@mui/material/Tab";
 import TabContext from "@mui/lab/TabContext";
 import TabList from "@mui/lab/TabList";
 import TabPanel from "@mui/lab/TabPanel";
 
-import Main from "./Main";
-
-import data from "./data";
-
-function App() {
+function Main({ depth2 }) {
   const [value, setValue] = useState(1);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
-
-  const depth1 = Object.keys(data);
-  console.log(depth1);
+  const depth2Keys = Object.keys(depth2);
 
   return (
-    <div className="App">
+    <div>
       <Box sx={{ width: "100%", typography: "body1" }}>
-        <TabContext value={value} default={1}>
+        <TabContext value={value}>
           <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
             <TabList onChange={handleChange} aria-label="lab API tabs example">
-              {depth1.map((elem, index) => {
+              {depth2Keys.map((elem, index) => {
                 return <Tab label={elem} value={index + 1} />;
               })}
             </TabList>
           </Box>
-          {depth1.map((elem, index) => {
+          {depth2Keys.map((elem, index) => {
+            console.log("elem", elem, depth2[elem]);
             return (
               <TabPanel value={index + 1}>
-                <Main depth2={data[elem]} />
+                {Object.keys(depth2[elem]).length !== 0
+                  ? JSON.stringify(depth2[elem], null, 2)
+                  : "no value"}
               </TabPanel>
             );
           })}
@@ -44,4 +40,4 @@ function App() {
   );
 }
 
-export default App;
+export default Main;
